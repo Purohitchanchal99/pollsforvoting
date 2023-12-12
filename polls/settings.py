c77eca6fca838ed7785922fab91fefa62cec3f6f
purohitchanchal99 <chanchalpurohitt@gmail.com>
2023-12-12 16:52:23 +0530

    Initial commit

--- polls/settings.py
+++ polls/settings.py
@@ -122,10 +122,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# Additional directories where Django will look for static files.
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
 ]
+
+# Absolute filesystem path to the directory that will hold collected static files.
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+
+
+#
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
